4c7777a13ada976eab0be7a8de136ce764536946
Lera Bolonicheva <lerabolonicheva@gmail.com>
2024-05-28 13:19:42 +0200

    fix(style): sidebar (#18)
    Co-authored-by: Lera <lera@getstellar.ai>

--- common.py
+++ common.py
@@ -11,7 +11,7 @@ def set_page_container_style(padding_top=0, max_height=0, overflow="unset"):
       overflow: {overflow};
     }}
     h2 {{
-      padding-bottom: 0;
+      padding-bottom: 0px;
     }}
   </style>
   ''',
@@ -36,27 +36,30 @@ def set_sidebar_style():
   st.markdown(
     f"""
     <style>
-    [data-testid="stSidebarContent"] {{
-      max-height: 100vh !important;
-      overflow: hidden;
-    }}
-    [data-testid="stSidebarUserContent"] {{
-      padding: 15px;
-    }}
-    [data-testid="stSidebarNavItems"] {{
-      padding-top: 50px;
-    }}
-    [data-testid="stSidebarUserContent"] > div:nth-child(1) {{
-      max-height: 63vh;
-      overflow-y: scroll;
-    }}
-    [data-testid="stIFrame"] {{
-      position: fixed;
-      bottom: 10px;
-      max-height: 100px;
-      padding-top: 10px;
-      background: rgb(38, 39, 48);
-    }}
+      [data-testid="stSidebarContent"] {{
+        height: 100vh;
+        overflow: hidden;
+      }}
+      [data-testid="stSidebarUserContent"] {{
+        padding: 15px;
+        max-height: calc(100vh - 72px - 25vh);
+        overflow-y: scroll;
+      }}
+      [data-testid="stSidebarNavItems"] {{
+        padding-top: 50px;
+        height: 25vh;
+        padding: 15px 0;
+        display: flex;
+        flex-direction: column;
+        justify-content: end;
+      }}
+      [data-testid="stIFrame"] {{
+        position: fixed;
+        bottom: 10px;
+        max-height: 60px;
+        background: rgb(38, 39, 48);
+        padding-top: 10px;
+      }}
     </style>
   """,
     unsafe_allow_html=True)
